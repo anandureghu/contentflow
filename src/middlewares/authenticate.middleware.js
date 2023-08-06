e4f9cae27ab959a5642bf238100e3d1cb7aea172
anandureghu <anandukr145@gmail.com>
2023-08-06 19:08:27 +0530

    added more middleware logic

--- src/middlewares/authenticate.middleware.js
+++ src/middlewares/authenticate.middleware.js
@@ -2,6 +2,7 @@ const jwt = require("jsonwebtoken");
 const config = require("../config/config");
 const { ROLES } = require("../utils/constants");
 const httpStatus = require("http-status");
+const { handleError } = require("../common/exception/handle.exception");
 
 module.exports = (req, res, next) => {
   // Get the token from the request headers
@@ -17,14 +18,26 @@ module.exports = (req, res, next) => {
   try {
     // Verify the token
     const decoded = jwt.verify(token, config.app.jwt_secret);
+    const user = decoded.user;
 
     // Add the decoded user to the request object
-    req.user = decoded.user;
+    req.user = user;
     if (user.role === ROLES.VIEWER && req.method !== "GET") {
       return res.status(httpStatus.FORBIDDEN).json({ msg: "access denied" });
     }
+    if (
+      user.role === ROLES.EDITOR &&
+      (req.method === "DELETE" || req.method === "OPTIONS")
+    ) {
+      return res.status(httpStatus.FORBIDDEN).json({ msg: "access denied" });
+    }
+
+    if (req.baseUrl === "/api/v1/users" && user.role == ROLES.VIEWER) {
+      return res.status(httpStatus.FORBIDDEN).json({ msg: "access denied" });
+    }
+
     next();
   } catch (err) {
-    res.status(httpStatus.UNAUTHORIZED).json({ msg: "authorization denied" });
+    handleError(err);
   }
 };
